b4ae92030145a9096c16a1cc99a06eacde56cd45
Milen Tecle <m_tecle@hotmail.com>
2023-11-30 15:16:17 +0000

    Arrow functions walkthrough

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,31 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b
+}
+let sum = addTwoNumbers (3, 5)
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a,b) => (a + b); also a valid way to write it
+const addTwoNumbers2 = (a,b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('hello');
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines());
