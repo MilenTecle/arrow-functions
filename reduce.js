5154563c321b439105a0c31c1c6b7d681eb92a2e
Milen Tecle <m_tecle@hotmail.com>
2023-12-01 20:33:15 +0000

    Walkthrough reduce and filter method

---
+++ reduce.js
@@ -0,0 +1,35 @@
+
+// Summing an array of numbers:
+
+const nums = [0, 1, 2, 3, 4]
+let sum = nums.reduce((acc, curr) => acc + curr);
+console.log(sum);
+
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+
+  // Totaling a specific object property
+
+
+  // Grouping by a property, and totaling it too
